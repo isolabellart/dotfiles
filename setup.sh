aab4dcbde0d499243eb9e35a8fbe3fac3bdd5266
Josh Burns <contact@jbernsie.xyz>
2019-07-17 22:22:21 +1000

    xbps package installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
 STOW=$(which stow)
+PKGMANIFEST=./package_manifest.txt
+
+sudo -v
+
+sudo xbps-install -Syu
+
+while IFS= read -r line
+do
+  sudo xbps-install -y "$line"
+done < "$PKGMANIFEST"
 
 $STOW pkg -t $HOME --restow
+$STOW wallpaper -t $HOME/Pictures/Wal/ --restow
